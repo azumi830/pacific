3122e3368a506d51a92fbfa885c848b339090ed3
Osako Azumi <osako.azumi@lancers.co.jp>
2015-11-23 22:15:37 +0900

    sidebar-top作成

---
+++ sidebar-top.php
@@ -0,0 +1,79 @@
+      <section id="sidebar">
+        <aside class="rss_link">
+          <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/btn_rss_feed.png" width="250" height="28" alt="RSS" /></a>
+        </aside>
+        <div id="primary" class="widget-area">
+          <aside id="event-info" class="news-list">
+            <h1>イベント開催情報</h1>
+            <div class="info-wrap">
+              <ul>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">「バンコクロイヤルガーデンフェア」開催のお知らせ</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/bangkok_garden-61x61.jpg" alt="「バンコクロイヤルガーデンフェア」開催のお知らせ" /></a>
+                  <p>7月30日13時よりチャオプラヤモール1階噴水広場にて、スア &#8230;</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">「汐留 味紀行」開催のお知らせ</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/shiodome_restaurants-61x61.jpg" alt="「汐留 味紀行」開催のお知らせ" /></a>
+                  <p>いよいよ食欲の秋、秋の味覚をいちはやく取り入れたお食事を、汐 &#8230;</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">「古都ジョグジャカルタ物産展」開催のお知らせ</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/jogja_exhibition-61x61.jpg" alt="「古都ジョグジャカルタ物産展」開催のお知らせ" /></a>
+                  <p>ジャカルタ・タムリンモールで10月1日より1ヶ月間、「古都ジ &#8230;</p>
+                </li>
+              </ul>
+              <span class="link-text"><a href="#">イベント開催情報一覧</a></span>
+            </div><!-- .info-wrap end -->
+          </aside><!-- #event-info end -->
+          <aside id="malls-info" class="news-list">
+            <h1>新規モール出店情報</h1>
+            <div class="info-wrap">
+              <ul>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">ホノルルに「カメハメハモール」オープン</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/kamehameha_mall-61x61.jpg" alt="ホノルルに「カメハメハモール」オープン" /></a>
+                  <p>2010年7月、アメリカ合衆国ハワイ州ホノルル市のビーチウォ &#8230;</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">サンフランシスコ・ノブヒル地区に「ゴールデンゲートモール」オープン</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/golden-gate_mall-61x61.jpg" alt="サンフランシスコ・ノブヒル地区に「ゴールデンゲートモール」オープン" /></a>
+                  <p>2011年5月、アメリカ合衆国カリフォルニア州サンフランシス &#8230;</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">マレーシア・クアラルンプールに「オリエントモール」オープン</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/orient_mall-61x61.jpg" alt="マレーシア・クアラルンプールに「オリエントモール」オープン" /></a>
+                  <p>2012年4月、マレーシア・クアラルンプールに「オリエントモ &#8230;</p>
+                </li>
+              </ul>
+              <span class="link-text"><a href="#">モール出店情報一覧</a></span>
+            </div><!-- .info-wrap end -->
+          </aside><!-- #malls-info end -->
+          <aside id="information-info" class="news-list">
+            <h1>お知らせ</h1>
+            <div class="info-wrap">
+              <ul>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">人材募集のお知らせ</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/recruit-61x61.jpg" alt="人材募集のお知らせ" /></a>
+                  <p>商業施設の開発・運営を通じて地域への貢献を目指す当社では、自 &#8230;</p>
+                </li>
+                <li>
+                  <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
+                  <h2><a href="#">ホノルル支店を開設いたしました。</a></h2>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/honolulu_office-61x61.jpg"  alt="ホノルル支店を開設いたしました。" /></a>
+                  <p>2010年2月、アメリカ合衆国ハワイ州ホノルル市にパシフィッ &#8230;</p>
+                </li>
+              </ul>
+              <span class="link-text"><a href="#">お知らせ一覧</a></span>
+            </div><!-- .info-wrap end -->
+          </aside><!-- #information-info end -->
+        </div><!-- #primary end -->
+      </section><!-- #sidebar end  -->
