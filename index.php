f4c3c9a9707b48ef3fc7c23b0b74fb50ff187960
Osako Azumi <osako.azumi@lancers.co.jp>
2015-11-23 22:00:27 +0900

    imgのパスを変更

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>パシフィックモール開発株式会社 | 世界の地域と人とを結ぶ、パシフィックモール開発株式会社</title>
-<link rel="apple-touch-icon" href="./images/touch-icon.png" />
-<link rel="shortcut icon" href="./images/favicon.ico" />
+<link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/images/touch-icon.png" />
+<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
 <!--[if lt IE 9]>
   <meta http-equiv="Imagetoolbar" content="no" />
@@ -19,7 +19,7 @@
   <div id="container">
     <header id="header">
       <h1 id="site-id">
-        <a href="#"><img src="./images/header/site_id.png" alt="パシフィックモール開発株式会社" /></a>
+        <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/header/site_id.png" alt="パシフィックモール開発株式会社" /></a>
       </h1><!-- #site-id end -->
       <div id="utility-group">
         <nav id="utility-nav">
@@ -58,7 +58,7 @@
       </ul><!-- #menu-global end -->
     </nav><!-- #global-nav end -->
     <section id="branding">
-      <img src="./images/header/default.png" width="950" height="295" alt="" />
+      <img src="<?php bloginfo('template_url'); ?>/images/header/default.png" width="950" height="295" alt="" />
     </section><!-- #branding end -->
     <section id="contents-body">
       <section id="contents">
@@ -66,7 +66,7 @@
           <div class="malls-group">
             <article>
               <h1><a href="#">汐留モール</a></h1>
-              <a href="#"><img width="302" height="123" src="./images/top/shiodome_mall-302x123.jpg"  alt="汐留モール" /></a>
+              <a href="#"><img width="302" height="123" src="<?php bloginfo('template_url'); ?>/images/top/shiodome_mall-302x123.jpg"  alt="汐留モール" /></a>
               <p>「東洋のマンハッタン」といわれる東京汐留。汐留シオサイト内に位置し浜離宮恩賜庭園に隣接する交通至便の &#8230;</p>
               <div class="continue-button">
                 <a href="#">詳しく見る</a>
@@ -74,7 +74,7 @@
             </article>
             <article>
               <h1><a href="#">ドリアンモール</a></h1>
-              <a href="#"><img width="302" height="123" src="./images/top/durian_mall-302x123.jpg" alt="ドリアンモール" /></a>
+              <a href="#"><img width="302" height="123" src="<?php bloginfo('template_url'); ?>/images/top/durian_mall-302x123.jpg" alt="ドリアンモール" /></a>
               <p>シンガポールの中心街・オーチャードロードに古くからある小規模店舗群を時代の変化に合わせて整備・活性化 &#8230;</p>
               <div class="continue-button">
                 <a href="#">詳しく見る</a>
@@ -84,7 +84,7 @@
           <div class="malls-group">
             <article>
               <h1><a href="#">チャオプラヤモール</a></h1>
-              <a href="#"><img width="302" height="123" src="./images/top/chao-phraya_mall-302x123.jpg" alt="チャオプラヤモール" /></a>
+              <a href="#"><img width="302" height="123" src="<?php bloginfo('template_url'); ?>/images/top/chao-phraya_mall-302x123.jpg" alt="チャオプラヤモール" /></a>
               <p>チャオプラヤモールは、マハラート通りとタイワング通りの交差する交通至便な場所に建設され2007年に開 &#8230;</p>
               <div class="continue-button">
                 <a href="#">詳しく見る</a>
@@ -92,7 +92,7 @@
             </article>
             <article>
               <h1><a href="#">タムリンモール</a></h1>
-              <a href="#"><img width="302" height="123" src="./images/top/thamrin_mall-302x123.jpg" alt="タムリンモール" /></a>
+              <a href="#"><img width="302" height="123" src="<?php bloginfo('template_url'); ?>/images/top/thamrin_mall-302x123.jpg" alt="タムリンモール" /></a>
               <p>ジャカルタ中心部、BANK INDONESIA（インドネシア銀行）や日本大使館が建ち並ぶジャランタム &#8230;</p>
               <div class="continue-button">
                 <a href="#">詳しく見る</a>
@@ -107,14 +107,14 @@
             <article class="column-article" >
               <h1 class="update-title"><a href="#">マカロニ・スクータル</a></h1>
               <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
-              <a href="#"><img width="90" height="90" src="./images/top/macaroni-90x90.jpg" alt="マカロニ・スクータル" /></a>
+              <a href="#"><img width="90" height="90" src="<?php bloginfo('template_url'); ?>/images/top/macaroni-90x90.jpg" alt="マカロニ・スクータル" /></a>
               <p>今日はスタッフAさんのお姉さんが、かわいい小さな娘さん二人を &#8230;</p>
               <span class="link-text"><a href="#">続きを読む</a></span>
             </article>
             <article class="column-article" >
               <h1 class="update-title"><a href="#">汐留モール夏祭りの花火大会</a></h1>
               <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
-              <a href="#"><img width="90" height="90" src="./images/top/fireworks-90x90.png" alt="汐留モール夏祭りの花火大会" /></a>
+              <a href="#"><img width="90" height="90" src="<?php bloginfo('template_url'); ?>/images/top/fireworks-90x90.png" alt="汐留モール夏祭りの花火大会" /></a>
               <p>先週金曜日の夕方、第9回汐留モール夏祭りが開催されました。ご &#8230;</p>
               <span class="link-text"><a href="#">続きを読む</a></span>
             </article>
@@ -123,14 +123,14 @@
             <article class="column-article" >
               <h1 class="update-title"><a href="#">ジョグジャの街</a></h1>
               <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
-              <a href="#"><img width="90" height="90" src="./images/top/jogjakarta-90x90.jpg" alt="ジョグジャの街"/></a>
+              <a href="#"><img width="90" height="90" src="<?php bloginfo('template_url'); ?>/images/top/jogjakarta-90x90.jpg" alt="ジョグジャの街"/></a>
               <p>10月に開催される物産展の準備のために、ジョグジャカルタ（以 &#8230;</p>
               <span class="link-text"><a href="#">続きを読む</a></span>
             </article>
             <article class="column-article" >
               <h1 class="update-title"><a href="#">アジアのハブ空港、チャンギ</a></h1>
               <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
-              <a href="#"><img width="90" height="90" src="./images/top/changi_airport-90x90.jpg" alt="アジアのハブ空港、チャンギ" /></a>
+              <a href="#"><img width="90" height="90" src="<?php bloginfo('template_url'); ?>/images/top/changi_airport-90x90.jpg" alt="アジアのハブ空港、チャンギ" /></a>
               <p>ドリアンモールがお手本にしたいシンガポール国内のNo.1は、 &#8230;</p>
               <span class="link-text"><a href="#">続きを読む</a></span>
             </article>
@@ -139,7 +139,7 @@
       </section><!-- #contents end -->
       <section id="sidebar">
         <aside class="rss_link">
-          <a href="#"><img src="./images/btn_rss_feed.png" width="250" height="28" alt="RSS" /></a>
+          <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/btn_rss_feed.png" width="250" height="28" alt="RSS" /></a>
         </aside>
         <div id="primary" class="widget-area">
           <aside id="event-info" class="news-list">
@@ -149,19 +149,19 @@
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">「バンコクロイヤルガーデンフェア」開催のお知らせ</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/bangkok_garden-61x61.jpg" alt="「バンコクロイヤルガーデンフェア」開催のお知らせ" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/bangkok_garden-61x61.jpg" alt="「バンコクロイヤルガーデンフェア」開催のお知らせ" /></a>
                   <p>7月30日13時よりチャオプラヤモール1階噴水広場にて、スア &#8230;</p>
                 </li>
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">「汐留 味紀行」開催のお知らせ</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/shiodome_restaurants-61x61.jpg" alt="「汐留 味紀行」開催のお知らせ" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/shiodome_restaurants-61x61.jpg" alt="「汐留 味紀行」開催のお知らせ" /></a>
                   <p>いよいよ食欲の秋、秋の味覚をいちはやく取り入れたお食事を、汐 &#8230;</p>
                 </li>
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">「古都ジョグジャカルタ物産展」開催のお知らせ</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/jogja_exhibition-61x61.jpg" alt="「古都ジョグジャカルタ物産展」開催のお知らせ" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/jogja_exhibition-61x61.jpg" alt="「古都ジョグジャカルタ物産展」開催のお知らせ" /></a>
                   <p>ジャカルタ・タムリンモールで10月1日より1ヶ月間、「古都ジ &#8230;</p>
                 </li>
               </ul>
@@ -175,19 +175,19 @@
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">ホノルルに「カメハメハモール」オープン</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/kamehameha_mall-61x61.jpg" alt="ホノルルに「カメハメハモール」オープン" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/kamehameha_mall-61x61.jpg" alt="ホノルルに「カメハメハモール」オープン" /></a>
                   <p>2010年7月、アメリカ合衆国ハワイ州ホノルル市のビーチウォ &#8230;</p>
                 </li>
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">サンフランシスコ・ノブヒル地区に「ゴールデンゲートモール」オープン</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/golden-gate_mall-61x61.jpg" alt="サンフランシスコ・ノブヒル地区に「ゴールデンゲートモール」オープン" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/golden-gate_mall-61x61.jpg" alt="サンフランシスコ・ノブヒル地区に「ゴールデンゲートモール」オープン" /></a>
                   <p>2011年5月、アメリカ合衆国カリフォルニア州サンフランシス &#8230;</p>
                 </li>
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">マレーシア・クアラルンプールに「オリエントモール」オープン</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/orient_mall-61x61.jpg" alt="マレーシア・クアラルンプールに「オリエントモール」オープン" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/orient_mall-61x61.jpg" alt="マレーシア・クアラルンプールに「オリエントモール」オープン" /></a>
                   <p>2012年4月、マレーシア・クアラルンプールに「オリエントモ &#8230;</p>
                 </li>
               </ul>
@@ -201,13 +201,13 @@
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">人材募集のお知らせ</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/recruit-61x61.jpg" alt="人材募集のお知らせ" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/recruit-61x61.jpg" alt="人材募集のお知らせ" /></a>
                   <p>商業施設の開発・運営を通じて地域への貢献を目指す当社では、自 &#8230;</p>
                 </li>
                 <li>
                   <time class="entry-date" datetime="2012-01-01">2012年1月1日</time>
                   <h2><a href="#">ホノルル支店を開設いたしました。</a></h2>
-                  <a href="#"><img width="61" height="61" src="./images/top/honolulu_office-61x61.jpg"  alt="ホノルル支店を開設いたしました。" /></a>
+                  <a href="#"><img width="61" height="61" src="<?php bloginfo('template_url'); ?>/images/top/honolulu_office-61x61.jpg"  alt="ホノルル支店を開設いたしました。" /></a>
                   <p>2010年2月、アメリカ合衆国ハワイ州ホノルル市にパシフィッ &#8230;</p>
                 </li>
               </ul>
