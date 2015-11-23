cd8444014b89a91de5a9f6b9afad38bef5785d1e
Osako Azumi <osako.azumi@lancers.co.jp>
2015-11-23 22:17:57 +0900

    footer作成

---
+++ footer.php
@@ -0,0 +1,10 @@
+    </section><!-- #contents-body end -->
+  </div><!-- #container end -->
+  <div id="footer-container">
+    <footer id="footer">
+      <p id="copyright"><small>Copyright &copy; 2011 <a href="#">パシフィックモール開発株式会社</a> All rights reserved.</small></p>
+    </footer><!-- #footer end -->
+  </div><!-- #footer-container end -->
+</div><!-- #wrap end -->
+</body>
+</html>
--- index.php
+++ index.php
@@ -76,13 +76,4 @@
         </section><!-- #latest-columns end -->
       </section><!-- #contents end -->
 <?php get_sidebar('top'); ?>
-    </section><!-- #contents-body end -->
-  </div><!-- #container end -->
-  <div id="footer-container">
-    <footer id="footer">
-      <p id="copyright"><small>Copyright &copy; 2011 <a href="#">パシフィックモール開発株式会社</a> All rights reserved.</small></p>
-    </footer><!-- #footer end -->
-  </div><!-- #footer-container end -->
-</div><!-- #wrap end -->
-</body>
-</html>
+<?php get_footer(); ?>
